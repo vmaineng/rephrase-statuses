b4dac668b0221ed9408e806f7cf0fab33b52510a
Mai Vang <vmaineng@gmail.com>
2025-05-13 12:21:15 -0700

    removed print

--- rephrase_project_backend/rephrase_project_backend/settings.py
+++ rephrase_project_backend/rephrase_project_backend/settings.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(os.path.join(BASE_DIR, '.env'))
-print("OPENAI_API_KEY from settings:", os.environ.get('OPENAI_API_KEY'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
